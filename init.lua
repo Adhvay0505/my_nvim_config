f689a626ce5fb0350566a6ed3f5747b4ea2b83f8
./adhv4y <149415126+Adhvay0505@users.noreply.github.com>
2025-07-29 23:39:23 +0530

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Enable line numbers on the left side of the editor
+vim.opt.number = true
+
+-- Tell Neovim that your terminal background is dark
+vim.opt.background = "dark"
+
+-- Set the colorscheme to 'elflord'
+vim.cmd("colorscheme elflord")
+
+-- Highlight the line where the cursor is
+vim.opt.cursorline = true
