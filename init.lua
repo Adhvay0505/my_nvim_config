16c4b441d43d522b2a5b9ff22b06f8325131f861
./adhv4y <149415126+Adhvay0505@users.noreply.github.com>
2025-08-06 17:56:18 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,3 +9,9 @@ vim.cmd("colorscheme elflord")
 
 -- Highlight the line where the cursor is
 vim.opt.cursorline = true
+
+-- Enable horizontal scrolling
+vim.opt.wrap = false          -- Disable line wrapping
+vim.opt.sidescroll = 1        -- Scroll 1 column at a time when cursor hits edge
+vim.opt.sidescrolloff = 8     -- Keep 8 columns visible around the cursor
+
