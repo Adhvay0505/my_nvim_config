0188d52cd1c7d22d0a122a5fe7a76e31b2835c68
./adhv4y <149415126+Adhvay0505@users.noreply.github.com>
2025-08-14 12:57:35 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -11,4 +11,4 @@ vim.opt.cursorline = true                          -- Highlight current line
 vim.opt.wrap = false                               -- Don't wrap lines
 vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
 vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
-vim.opt.clipboard = "unnamedplus"		               -- Use system clipboard
+vim.opt.clipboard = "unnamedplus"                  -- Use system clipboard
